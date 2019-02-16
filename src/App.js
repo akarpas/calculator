6b55f4d4b7b643ee071bc4d7f938d37fbaa00ee4
Andreas Karpasitis <karpasitis@outlook.com>
2019-02-16 21:29:51 +0100

    create basic button structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import buttons from "./data/buttons.json";
 import style from './App.module.scss';
 
 class App extends Component {
@@ -8,6 +9,8 @@ class App extends Component {
 
   render() {
     const { input } = this.state;
+    const { numbers, operations, other } = buttons;
+
     return (
       <div className={style.app}>
         <div className={style.calculator}>
@@ -17,6 +20,25 @@ class App extends Component {
               type="text"
               value={input}
             />
+          </div>
+          <div className={style.buttons}>
+            <div className={style.left}>
+              <div className={style.other}>
+                {other.map(button =>
+                  <button id={button.description} className={style.buttonOther}>{button.sign}</button>)}
+              </div>
+              <div className={style.numbers}>
+                {numbers.map(number =>
+                  <button id={number} className={number === 0
+                    ? style.buttonNumberZero : style.buttonNumber}>
+                    {number}
+                  </button>)}
+              </div>
+            </div>
+            <div className={style.right}>
+              {operations.map(operation => 
+                <button className={style.buttonOperation}>{operation}</button>)}
+            </div>
           </div>
         </div>
       </div>
