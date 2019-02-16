a28be5e22750740369e62d919035e3d9daa575b5
Andreas Karpasitis <karpasitis@outlook.com>
2019-02-16 22:04:46 +0100

    add basic operations and numbers logic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,64 @@ import style from './App.module.scss';
 
 class App extends Component {
   state = {
-    input: 0,
+    input: null,
+    memory: null,
+    firstNumber: null,
+    operation: null,
+    newInput: false,
+  }
+
+  handleClick = (e, type) => {
+    e.preventDefault();
+    const { id } = e.target;
+    const {
+      input,
+      firstNumber,
+      operation,
+      newInput
+    } = this.state;
+
+    switch (type) {
+      case "other":
+        break;
+      case "number":
+        if (operation && firstNumber) {
+          this.setState({
+            input: newInput ? id : input + id,
+            newInput: newInput && false,
+          });
+        } else {
+          this.setState({ input: !input ? id : input + id });
+        }
+        break;
+      case "operation":
+        if (id === '=') {
+          this.setState({ input: this.calculate(operation, firstNumber, input) })
+        }
+        if (input) {
+          this.setState({ operation: id, firstNumber: input, newInput: true })
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  calculate = (operation, firstNumber, input) => {
+    const firstNumberInt = Number(firstNumber);
+    const secondNumberInt = Number(input);
+    switch (operation) {
+      case "/":
+        return firstNumberInt / secondNumberInt;
+      case "*":
+        return firstNumberInt * secondNumberInt;
+      case "+":
+        return firstNumberInt + secondNumberInt;
+      case "-":
+        return firstNumberInt - secondNumberInt;
+      default:
+        break;
+    }
   }
 
   render() {
@@ -18,26 +75,26 @@ class App extends Component {
             <input
               className={style.input}
               type="text"
-              value={input}
+              value={!input ? '0' : input}
             />
           </div>
           <div className={style.buttons}>
             <div className={style.left}>
               <div className={style.other}>
                 {other.map(button =>
-                  <button id={button.description} className={style.buttonOther}>{button.sign}</button>)}
+                  <button onClick={e => this.handleClick(e, 'other')} id={button.description} className={style.buttonOther}>{button.sign}</button>)}
               </div>
               <div className={style.numbers}>
                 {numbers.map(number =>
-                  <button id={number} className={number === 0
+                  <button onClick={e => this.handleClick(e, 'number')} id={number} className={number === 0
                     ? style.buttonNumberZero : style.buttonNumber}>
                     {number}
                   </button>)}
               </div>
             </div>
             <div className={style.right}>
-              {operations.map(operation => 
-                <button className={style.buttonOperation}>{operation}</button>)}
+              {operations.map(operation =>
+                <button id={operation} onClick={e => this.handleClick(e, 'operation')} className={style.buttonOperation}>{operation}</button>)}
             </div>
           </div>
         </div>
